d9b8b5cea9973b354dabc2a82fa639ef4ed221b4
Shafi23 <105143502+Shafi23@users.noreply.github.com>
2026-02-20 10:14:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-051a31ab2f4d498f5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Simple-CICD-Server"
+  }
+}
